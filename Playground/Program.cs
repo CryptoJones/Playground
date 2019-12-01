37305ae32630bd0e2d9a3fa8d4dde69fa8b02b09
Aaron K. Clark <akclark@atbashservices.com>
2019-12-01 01:10:29 -0600

    Puzzle One Completed.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -10,6 +10,37 @@ namespace Playground
     {
         static void Main(string[] args)
         {
+            puzzleOne();
+        }
+
+        public static void puzzleOne()
+        {
+            var inputs = new List<int>();
+
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"Puzzle1Inputs.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                int result;
+                Int32.TryParse(line, out result);
+                inputs.Add(result);
+                counter++;
+            }
+
+            file.Close();
+
+            int sumOfInts = 0;
+            foreach (int i in inputs)
+            {
+                int working = i / 3;
+                working = working - 2;
+                sumOfInts = sumOfInts + working;
+            }
+
+            Console.WriteLine("Done! The answer is " + sumOfInts.ToString());
+            Console.ReadLine();
         }
     }
 }
