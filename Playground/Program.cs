37e611bf501e94147946cbb0e5723e101f95d057
Aaron K. Clark <akclark@atbashservices.com>
2019-12-01 13:47:14 -0600

    Day 1 Part 2

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -10,7 +10,7 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            puzzleOne();
+            puzzleOnePartTwo();
         }
 
         public static void puzzleOne()
@@ -39,6 +39,51 @@ namespace Playground
                 sumOfInts = sumOfInts + working;
             }
 
+            Console.WriteLine("Done! The answer is " + sumOfInts.ToString());
+            Console.ReadLine();
+        }
+
+        public static void puzzleOnePartTwo()
+        {
+            var inputs = new List<int>();
+
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"Puzzle1Inputs.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                int result;
+                Int32.TryParse(line, out result);
+                inputs.Add(result);
+                counter++;
+            }
+
+            file.Close();
+
+            double sumOfInts = 0;
+            foreach (int i in inputs)
+            {
+                fuelcost(i);
+            }
+
+            Console.WriteLine("Done! The answer is " + sumOfInts.ToString());
+            Console.ReadLine();
+
+            void fuelcost(double initial)
+            {
+                double working = Math.Floor(initial / 3);
+                working = working - 2;
+                if (working > 0)
+                {
+                    sumOfInts = sumOfInts + working;
+                }
+
+                if (working > 0)
+                {
+                    fuelcost(working);
+                }
+            }
             Console.WriteLine("Done! The answer is " + sumOfInts.ToString());
             Console.ReadLine();
         }
